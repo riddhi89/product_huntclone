2cbab6840d1d6e056c140020299f90fd80a92b94
Riddhi Shah <ridhi.j.shah@gmail.com>
2018-12-30 21:05:14 -0800

    Finish up the website

--- product_huntlcone/urls.py
+++ product_huntlcone/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from products import views as product_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', product_views.home, name='home'),
     path('accounts/', include('accounts.urls')),
-]
+    path('products/', include('products.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
